1a5a6e258ded0929c053a73a37d63a00b6dd23c4
4a50 <jpjones22@gmail.com>
2020-12-09 23:02:02 -0800

    Add project files.

---
+++ Server/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var echoServer = new EchoServer();
+            echoServer.Start();
+
+            Console.WriteLine("Echo Server is Echoing");
+            Console.ReadLine();
+        }
+    }
+}
